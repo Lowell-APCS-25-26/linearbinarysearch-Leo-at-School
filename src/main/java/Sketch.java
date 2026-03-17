d78df09a84c4fbc7c8bf835af2ecccbe0f92fca8
Leo-at-School <153330136+Leo-at-School@users.noreply.github.com>
2026-03-17 16:25:45 +0000

    first versions of all the searching algorithms

--- src/main/java/Sketch.java
+++ src/main/java/Sketch.java
@@ -24,20 +24,58 @@ public class Sketch {
     new Item(19967, 45)
   };                             
   public int linearSearch(int catNumToFind){
-    //complete this method
+    for (int i = 0; i < store.length; i++){
+      if (Store[i].getCatNum() == catNumToFind){
+        return i;
+      }
+    }
+
     return -1;
   }
   public int recursiveLinearSearch(int catNumToFind, int startIndex){
-    //complete this method
-    return -1;
+    if (startIndex >= store.length){
+      return -1;
+    } else if (Store[startIndex].getCatNum() == catNumToFind){
+      return startIndex;
+    }
+
+    return recursiveLinearSearch(catNumToFind + 1);
   }
   public int binarySearch(int catNumToFind){
-    //complete this method    
+    int low = 0;
+    int high = store.length - 1;
+    int guess;
+
+    while(low <= high){
+      guess = (low + high)/2;
+
+      if (store[guess].getCatNum() == catNumToFind){
+        return guess;
+      }
+
+      if (catNumToFind < store[guess].getCatNum()){
+        high = guess + 1;
+      } else {
+        low = guess - 1;
+      }
+    }
+
     return -1;
   }
   public int recursiveBinarySearch(int catNumToFind, int nLow, int nHigh){
-    //complete this method    
-    return -1;
+      int guess = (low + high)/2;
+
+      if (store[guess].getCatNum() == catNumToFind){
+        return guess;
+      } else if (low > high){
+        return -1;
+      }
+
+      if (store[guess].getCatNum() < catNumToFind){
+        return recursiveBinarySearch(catNumToFind, nLow, guess + 1);
+      }
+
+      return recursiveBinarySearch(catNumToFind, guess - 1, nHigh);
   }
   public void tester(){
     int[] tests = {0, 183, 184, 2370, 15320, 19967, 19968};
